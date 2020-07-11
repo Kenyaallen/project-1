9381ac4a97ace9c63c6460cd846a7aca35f6422a
Sarah Kerr <sarah.kerr93@gmail.com>
2020-07-11 13:51:06 -0400

    compiling matching data from both APIs into one list for display

--- assets/script.js
+++ assets/script.js
@@ -5,8 +5,12 @@ $(document).ready(function(){
     // var latitude; 
     // var longitude;
     var zipCode; 
-    var restaurantLat; 
-    var restaurantLong; 
+    // var restaurantLat; 
+    // var restaurantLong; 
+    var restaurantArray = []; 
+    var refugeeArray= []; 
+    var restaurantResponse;  
+    var intersection; 
 
 function getZipCode(){ 
   zipCode = $(".validate").val()
@@ -27,48 +31,52 @@ function getLatLngByZipcode() {
     console.log(latitude, longitude); 
     getRestaurants(latitude, longitude); 
   })
-
-  // pullRefugeeInfo(); 
   
 }
 
 function getRestaurants (latitude, longitude){
   $.ajax({
     method: "GET",
-    url: "https://developers.zomato.com/api/v2.1/search?lat=" + latitude + "&lon=" + longitude, 
+    url: "https://developers.zomato.com/api/v2.1/search?lat=" + latitude + "&lon=" + longitude + "&count=100", 
     headers: {
         "user-key": ZomatoAPIKey,
-        "content-type": "application/json"
+        "content-type": "application/json", 
     }
     }).then(response => {
-        // console.log(response);
         console.log(response); 
-        displayRestaurants(response); 
+        restaurantResponse = response.restaurants; 
+        for (i=0; i<response.restaurants.length; i++){
+          restaurantArray.push(response.restaurants[i].restaurant.name); 
+        }
+
+        pullRefugeeInfo(latitude, longitude); 
+
     })
 }    
 
 function displayRestaurants (response){ 
-  for (i=0; i<10; i++){
-    var restaurant = $("<div>");
+  for (i=0; i<20; i++){
+    // var restaurant = $("<div>");
+
     // var restaurantBtn = $("<button>") 
     // restaurantBtn.attr("class", "btn-floating btn-small waves-effect waves-light"); 
     // restaurantBtn.attr("lat", response.restaurants[i].restaurant.location.latitude); 
     // restaurantBtn.attr("long", response.restaurants[i].restaurant.location.longitude); 
-    restaurant.attr("lat", response.restaurants[i].restaurant.location.latitude); 
-    restaurant.attr("long", response.restaurants[i].restaurant.location.longitude); 
-    restaurant.html(response.restaurants[i].restaurant.name);  
-    var restaurantLat = response.restaurants[i].restaurant.location.latitude; 
-    var restaurantLong = response.restaurants[i].restaurant.location.longitude; 
-    $(".searchResultsArea").append(restaurant);
-    // restaurant.append(restaurantBtn); 
-    pullRefugeeInfo(restaurantLat, restaurantLong); 
+    // restaurant.attr("lat", response.restaurants[i].restaurant.location.latitude); 
+    // restaurant.attr("long", response.restaurants[i].restaurant.location.longitude); 
+    // restaurant.html(response.restaurants[i].restaurant.name);  
+    // var restaurantLat = response.restaurants[i].restaurant.location.latitude; 
+    // var restaurantLong = response.restaurants[i].restaurant.location.longitude; 
+    // $(".searchResultsArea").append(restaurant);
+    // // restaurant.append(restaurantBtn); 
+    // pullRefugeeInfo(restaurantLat, restaurantLong); 
     
   }
 }
 
 //refugee api call 
 function pullRefugeeInfo(restaurantLat, restaurantLong){
-    var queryURL = "https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=1&per_page=10&offset=0&lat=" + restaurantLat + "&lng=" + restaurantLong;
+    var queryURL = "https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=1&per_page=20&offset=0&lat=" + restaurantLat + "&lng=" + restaurantLong;
 
     $.ajax({
     method: "GET",
@@ -76,22 +84,49 @@ function pullRefugeeInfo(restaurantLat, restaurantLong){
     headers: {
         "content-type": "application/json"
     }
-    }).then(response => {
-        console.log(response)
+    }).then(response => {   
+        console.log(response);   
+        refugeeResponse = response;    
+        for (i=0; i<20; i++){
+          refugeeArray.push(response[i].name); 
+        }
 
+        compareArrays(); 
 
     })
+}
+
+function compareArrays (){ 
+  console.log(restaurantArray); 
+  console.log(refugeeArray); 
+  intersection = refugeeArray.filter(function(e) {
+    return restaurantArray.indexOf(e) > -1;
+  });
+  console.log(intersection); 
+  finalRestaurantData();
+
+}
+
+function finalRestaurantData (){
+  var restaurantFinalData = restaurantResponse.filter(function(item){
+    return intersection.includes(item.restaurant.name)
+  })
+
+
+  var refugeeFinalData = refugeeResponse.filter(function(item){
+      return intersection.includes(item.name)
+
+  })
+  console.log(restaurantFinalData)
+  console.log(refugeeFinalData)
+
+  
 }
 
 
 $("#searchBtn").on("click", getZipCode)
 
-//not yet functional - need class or ID of each individual restaurant search result button 
-//$("add class or ID here").on("click", pullRefugeeInfo)
-
 }); 
-
-
 
 
 
